7f4a557447885878b1dbabd5c5956a6662d4ee31
Renan Stankevicius <renan.stankevicius@gmail.com>
2023-09-30 00:25:23 -0300

    corrige pastas e arquivos

--- GamingGroove.Data/Models/Comunidade.cs
+++
@@ -1,9 +0,0 @@
-namespace GamingGroove.Data.Models;
-
-public class Comunidade
-{
-    public int ComunidadeId { get; set; }
-    public string? Nome { get; set; }
-    public string? Descricao { get; set; }
-    public virtual ICollection<Usuario>? Membros { get; set; }  // Uma comunidade pode ter muitos membros.
-}
