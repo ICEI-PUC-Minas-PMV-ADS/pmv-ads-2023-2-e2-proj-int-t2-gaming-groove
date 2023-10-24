ae428749781979ee4949906ec1a26a8c235c01ee
Renan Stankevicius <renan.stankevicius@gmail.com>
2023-10-24 09:44:15 -0300

    fix: corrige layout Comunidades do perfil

--- src/GamingGroove/wwwroot/js/PerfilPage.js
+++ src/GamingGroove/wwwroot/js/PerfilPage.js
@@ -1,26 +1,16 @@
 const communityButtons = document.querySelectorAll(".user-communities-items");
+
 communityButtons.forEach(communityButton => {
     const communityHidden = communityButton.querySelector(".hidden");
 
     communityButton.addEventListener("click", function() {
-        if (communityHidden.style.display === 'flex') {
-            communityHidden.style.display = 'none';
-        } else {
-            communityHidden.style.display = 'flex';
-        }
+        communityButtons.forEach(button => {
+            const hidden = button.querySelector(".hidden");
+            hidden.style.display = 'none';
+            button.classList.remove('ativo');
+        });
+        communityHidden.style.display = 'flex';
+        communityButton.classList.add('ativo');
     });
 });
 
-
-
-communityButtons.forEach(button => {
-    button.addEventListener("click", () => {
-        const hiddenContent = button.querySelector(".hidden");
-
-        if (hiddenContent.style.display === "flex") {
-            hiddenContent.style.marginBottom = "20px";
-        } else {
-            hiddenContent.style.marginBottom = "0";
-        }
-    });
-});
