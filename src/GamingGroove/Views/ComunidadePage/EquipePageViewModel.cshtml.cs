4e776169d9f4e4de84d5e344f124eda65876e884
shigerysasaki <shigerysasaki12@hotmail.com>
2023-10-25 21:57:38 -0300

    feat:cria comunidade PAGE

---
+++ src/GamingGroove/Views/ComunidadePage/EquipePageViewModel.cshtml.cs
@@ -0,0 +1,27 @@
+using GamingGroove.Data;
+using GamingGroove.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Security.Claims;
+
+namespace GamingGroove.Views.ComunidadePage
+{
+    public class ComunidadePageViewModel : PageModel
+    {
+        private readonly GamingGrooveDbContext _cc;
+
+        public ComunidadePageViewModel(GamingGrooveDbContext cc)
+        {
+            _cc = cc;
+        }
+
+        public IEnumerable<EquipeModel> getEquipes { get; set; }
+        public IEnumerable<UsuarioEquipeModel> getEquipesUsuario { get; set; }
+        
+        public void GetEquipesUsuario(int usuario)
+        {
+            
+        }
+    }
+}
